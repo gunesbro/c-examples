ce845b59e88981867642103af3a955754de1053c
gunesbro <gunes.burak@outlook.com.tr>
2022-11-26 12:46:59 +0300

    Add files via upload

---
+++ BenchmarkDotNet/BenchmarkDotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Text;
+
+namespace BenchmarkDotNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MyBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}
